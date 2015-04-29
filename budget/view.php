da92be9bac4eead3f3b9eaae6c46559f57ce7cd0
BryceFranzen <fra12032@byui.edu>
2015-04-29 15:07:51 -0600

    Initial Commit

---
+++ budget/view.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Budget</title>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/styles.php'; ?>
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/nav.php'; ?>
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                           Budget
+                            <small>Subheading</small>
+                        </h1>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/script.php'; ?>
+
+</body>
+
+</html>
